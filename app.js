31fb23e21bd34602c4da10853624c30cfefd849e
Krishna <krishna9336@gmail.com>
2023-09-17 22:21:23 +0530

    route fix

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.post('/api/users', async (req, res,next) => {
   }
 });
 
-app.get('/api/users', async (req, res,next) => {
-  try {
-    const users = await User.find();
+app.get('/api/users/all', async (req,res,next)=>{
+  try{
+  const users = await User.find();
     res.json(users);
   } catch (error) {
     next(error)  }
